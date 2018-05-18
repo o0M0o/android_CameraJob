693e3290dfa7c23368553cffb94fa005360b0b6a
hugo <fly_in_wind@qq.com>
2018-05-18 11:02:40 +0800

    just record

--- app/src/main/java/com/wxm/camerajob/utility/DlgUtility.kt
+++ app/src/main/java/com/wxm/camerajob/utility/DlgUtility.kt
@@ -1,14 +1,14 @@
 package com.wxm.camerajob.utility
 
-import android.app.Activity
 import android.app.AlertDialog
+import android.content.Context
 
 /**
  * @author      WangXM
  * @version     create：2018/5/14
  */
 object AlertDlgUtility {
-    fun showAlert(home: Activity, szTitle: String, szMsg: String) {
+    fun showAlert(home: Context, szTitle: String, szMsg: String) {
         AlertDialog.Builder(home)
                 .setTitle(szTitle).setMessage(szMsg)
                 .create().show()
